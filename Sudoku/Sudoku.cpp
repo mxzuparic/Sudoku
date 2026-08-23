47e9639289dee00f867b8747e8e9cd0f2665cfa8
Melita Župarić <meli.zuparic@gmail.com>
2026-08-23 15:35:16 +0200

    Add project files.

---
+++ Sudoku/Sudoku.cpp
@@ -0,0 +1,14 @@
+﻿#include <QApplication>
+#include <QWidget>
+
+int main(int argc, char* argv[])
+{
+    QApplication application(argc, argv);
+
+    QWidget window;
+    window.setWindowTitle("Sudoku");
+    window.resize(500, 600);
+    window.show();
+
+    return application.exec();
+}
